33b06b148509e36735c2e835c2ee65782c135b39
NetH1 <youtube_17@mail.ru>
2023-02-25 17:28:42 +0600

    c

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {FC, useState} from 'react';
 import HomeWork2302 from '../Components/HomeWork2302/HomeWork2302';
 import teacther from '../images/teacher.png'
 
@@ -15,7 +15,7 @@ type typeusersState = {
     image:string,
 }
 
-const HomePage: React.FC<HomePageProps> = () => {
+const HomePage:FC<HomePageProps> = () => {
     const [users, setUsers] = useState<typeusersState[]>([
         {id:1,name:"Laura Smith",profession:"Frontend Developer",website:"laurasmith.website", abouttext:"I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.", interesttext:"Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.", image:teacther},
         {id:2,name:"Laura Smith",profession:"Frontend Developer",website:"laurasmith.website", abouttext:"I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.", interesttext:"Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.", image:teacther},
@@ -23,18 +23,19 @@ const HomePage: React.FC<HomePageProps> = () => {
     ]);
     const [hide, setHide] = useState<boolean>(false)
 
-    const clickHandler = (id:number, e: React.MouseEvent<HTMLButtonElement | undefined, MouseEvent>):void =>{
+    const clickHandler = (id:number, e: React.MouseEvent<HTMLButtonElement | undefined>):void =>{
         const newUsers = [...users].map(user =>{
               if(user.id == id) {
                   setHide(true)
-              } else{setHide(false)}
+              }
+              return user
           })
           setUsers(newUsers)
       }
     return (
-        <>
+        <div>
             <HomeWork2302 users={users} clickHandler={clickHandler} hide={hide} />
-        </>
+        </div>
     );
 };
 
